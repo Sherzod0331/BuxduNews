9430b50f31ede858ba82e4278214355f4289d14f
Sherzod <87847981+Sherzod0331@users.noreply.github.com>
2023-03-08 11:34:42 +0500

    content

---
+++ Js/main.js
@@ -0,0 +1,34 @@
+function menu_active() {
+    document.querySelector("#menu .menu_ul").classList.toggle("active_menu");
+    document.querySelector("#menu .btn").classList.toggle("active_btn");
+    document.querySelector("#menu .fa-bars").classList.toggle("active_bars");
+    document.querySelector("#menu .fa-xmark").classList.toggle("active_xmark");
+}
+
+function eye(a) {
+    if (a==1) {
+        document.querySelector(".pass-slash").classList.toggle("active_bars");
+        document.querySelector(".pass").classList.toggle("active_xmark");
+        var x = document.getElementById("myInput_1");
+        if (x.type === "password") {
+            x.type = "text";
+        } else {
+            x.type = "password";
+        }
+    }
+
+    if (a==2) {
+        document.querySelector(".pass-res-slash").classList.toggle("active_bars");
+        document.querySelector(".pass-res").classList.toggle("active_xmark");
+        var x = document.getElementById("myInput_2");
+        if (x.type === "password") {
+            x.type = "text";
+        } else {
+            x.type = "password";
+        }
+    }
+}
+
+function close_message() {
+    document.querySelector(".message").classList.toggle("close_message");
+}
